754bf09a2f0843d81f49e59181a8ae110ffaf15a
OtavioCuriel <otavio.curiel345@gmail.com>
2026-03-30 01:42:17 -0300

    Segundo feito

---
+++ Apredendo c++/02_soma_dois_numeros.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Digite o primeiro numero: ";
+    cin >> a;
+    cout << "Digite o segundo numero: ";
+    cin >> b;
+
+    cout << "Soma = " << (a + b) << endl;
+    return 0;
+}
