7dd8edacccbc6d082c7ba740ff2a2af5b8b1aeca
brunor18 <brunoro343@gmail.com>
2026-04-10 20:53:11 -0300

    refactor: limpando o codigo

---
+++ Prova/prova-cs/Program.cs
@@ -0,0 +1,19 @@
+﻿struct Acao { 
+    int codigo; 
+    string descricao; 
+};
+
+struct Chamados
+{
+    int ID;
+    string nome;
+    string descricao;
+    Acao historico;
+
+};
+
+struct Fila
+{
+    string[] fila = new string[100]; 
+}
+
