d5067cf0a85807341682fb9df41b905a7e19b68d
brunor18 <brunoro343@gmail.com>
2026-04-17 21:34:58 -0300

    feat: criando base pro projeto

--- pointers2/Main.cpp
+++ pointers2/Main.cpp
@@ -2,7 +2,77 @@
 using namespace std;
 
 
+
+
+struct Produto{
+   int codigoProduto;
+   char nomePoduto[100];
+   char quantidade[100];
+   char unidadeDeMedida[100];
+};
+
+
+struct Estoque {
+   int codigo;
+   char nomeEstoque[100];
+   char dataVencimento[100];
+   char descricao[100];
+   Produto produto;
+};
+
+
+
+
+
 int main() {
-   cout << "Hello, World!" << endl;
-   return 0;
+   Estoque estoque[0];
+
+   int opcao;
+
+   while (true)
+   {
+      cout << "----Gerenciador de estoque----" ;
+      cout << "\n1. Criar estoque";
+      cout << "\n2. Adicionar produto ao um estoque";
+      cout << "\n3. Ver histórico";
+      cout << "\n4. Sair";
+      cout << "\nOpção:";
+      cin >> opcao;
+
+      switch (opcao)
+      {
+      case 1:
+         system("cls");
+         cout << "Teste";
+         break;
+      
+      case 2:
+         system("cls");
+         cout << "Teste2";
+         break;
+      
+      case 3:
+         system("cls");
+         cout << "Teste3";
+         break;
+      
+      case 4:
+         system("cls");
+         cout << "\nTeste 4";
+         break;
+      
+      default:
+         system("cls");
+         cout << "Saindo...";
+         exit(0);
+      }
+
+
+
+   }
+   
+   
+
+
+
 }
