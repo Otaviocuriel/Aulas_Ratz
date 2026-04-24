47a038378c1bd5a3d8fa639230fb8c51de3b9c0e
Bruno <brunoro343@gmail.com>
2026-04-24 20:47:55 -0300

    feat: criando funcao adicionarEstoque

--- pointers2/Main.cpp
+++ pointers2/Main.cpp
@@ -3,23 +3,41 @@ using namespace std;
 
 
 
-
 struct Produto{
-   int codigoProduto;
-   char nomePoduto[100];
-   char quantidade[100];
-   char unidadeDeMedida[100];
-};
+        int codigoProduto;
+        char nomePoduto[100];
+        char quantidade[100];
+        char unidadeDeMedida[100];
+    };
 
+    struct Estoque {
+        int codigo;
+        char nomeEstoque[100];
+        char dataVencimento[100];
+        char descricao[100];
+        Produto produto;
+    };
 
-struct Estoque {
-   int codigo;
-   char nomeEstoque[100];
-   char dataVencimento[100];
-   char descricao[100];
-   Produto produto;
-};
+Estoque adicionarEstoque(){
+      Estoque estoque;
+            
+      estoque.codigo = 1;
 
+      cout << "=== Criar Novo Estoque ===" << endl;
+        
+      cout << "Digite o nome do estoque: ";
+      cin >> estoque.nomeEstoque;
+        
+      cout << "Digite a data de vencimento (DD/MM/YYYY): ";
+      cin >> estoque.dataVencimento;
+        
+      cout << "Digite a descricao do estoque (opcional): ";
+      cin >> estoque.descricao;
+        
+      cout << "\nEstoque adicionado com sucesso!\n" << endl;
+        
+      return estoque;
+    }
 
 
 
@@ -43,8 +61,7 @@ int main() {
       switch (opcao)
       {
       case 1:
-         cout << "Teste";
-         break;
+         adicionarEstoque();
       
       case 2:
          cout << "Teste2";
